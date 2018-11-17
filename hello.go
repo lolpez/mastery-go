07c28df25c177d9957c1d7ccb739b70efae19821
lolpez <lopezsandi1666@gmail.com>
2018-11-17 16:15:37 -0400

    Code refactoring

--- hello.go
+++ hello.go
@@ -18,10 +18,11 @@ func main() {
 	if err != nil {
 		fmt.Print(err)
 	}
-	fileContent := string(b)             // convert file content into a 'string'
-	words := strings.Fields(fileContent) // insert each word into an array TODO: ignore special characters and process lower case
-	wordCounter := map[string]int32{}    //create a new dictionary to insert words with its counter
-	for _, element := range words {      //iterate through dictionary
+	fileContent := string(b)                          // convert file content into a 'string'
+	words := strings.Fields(fileContent)              // insert each word into an array TODO: ignore special characters and process lower case
+	wordCounter := map[string]int32{}                 //create a new dictionary to insert words with its counter
+	wordsSlice := make([]string, 0, len(wordCounter)) //create a new slice (dynamic array) for words
+	for _, element := range words {                   //iterate through dictionary
 		processedString := reg.ReplaceAllString(element, "") //remove special characters from string
 		if len(processedString) > 0 {                        //check if the processed word is empty
 			word := strings.ToLower(processedString) //convert word to lowercase
@@ -29,18 +30,13 @@ func main() {
 			if ok {
 				wordCounter[word] = value + 1 //if the word is already in the dictionary, then increment counter value
 			} else {
-				wordCounter[word] = 1 //else, insert a new word to the dictionary with initial counter value to 1
+				wordCounter[word] = 1                 //else, insert a new word to the dictionary with initial counter value to 1
+				wordsSlice = append(wordsSlice, word) //append word to words slice
 			}
 		}
 	}
-
-	keys := make([]string, 0, len(wordCounter)) //create a new array from dictionary keys
-	for k := range wordCounter {                //iterate through dictionary
-		keys = append(keys, k)
-	}
-	sort.Strings(keys) //sort array alphabetically
-
-	for _, k := range keys {
+	sort.Strings(wordsSlice) //sort array alphabetically
+	for _, k := range wordsSlice {
 		fmt.Println(k, wordCounter[k]) //print words counter
 	}
 }
