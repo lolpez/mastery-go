40bc1730dd529651a58582d2ffa37799a62b0521
lolpez <lopezsandi1666@gmail.com>
2018-11-17 15:35:34 -0400

    Added file reader

--- hello.go
+++ hello.go
@@ -2,8 +2,15 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 )
 
 func main() {
 	fmt.Println("hello world")
+	b, err := ioutil.ReadFile("file.txt")
+	if err != nil {
+		fmt.Print(err)
+	}
+	fileContent := string(b) // convert file content into a 'string'
+	fmt.Println(fileContent)
 }
